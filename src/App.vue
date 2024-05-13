da0b3d85ded69294a622a893d72977c0a66d938f
HugoFalci <hugo.flc.ribeiro@gmail.com>
2024-05-13 12:31:41 -0300

    Primeiros-passos

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <FormularioAcesso />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import FormularioAcesso from './components/FormularioAcesso.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    FormularioAcesso
+  }
+});
+</script>
+
+<style>
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="" class="logo">
+        </h1>
+    </header>
+</template>
+
+
+
+<script lang="ts">
+import { DefineComponent, defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+
+</script>
+
+
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #F0F0F0;
+  height: 25vw;
+  height: 100vh;
+  display: inline-block;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+.logo{
+    width: 50px;
+    height: auto;
+}
+</style>
